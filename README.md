b023d618b5eadda65101e5d4e91bbabd1ab53e4d
Andrew Wagner <andrew.wagner.pdx@gmail.com>
2022-06-15 18:02:21 -0700

    Got server-side validation of auth working

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ API service supporting the jobsearch webapp (https://github.com/joddyjods/jobsea
 - npm install express (dependency)
 - npm i --save cors (dependency)
 - node jobsearch-api.js
-
-
-# Things to do next
-- [ ] Expose an endpoint
+- npm install googleapis 
+      - https://github.com/googleapis/google-api-nodejs-client
+      - https://developers.google.com/identity/sign-in/web/sign-in
+- npm install @googleapis/docs
+- npm install google-auth-library --save
+- npm install jose (JWT library)
+- npm install google-oauth-jwt
--- jobsearch-api.js
+++ jobsearch-api.js
@@ -4,6 +4,42 @@ const cors = require('cors');
 const express = require('express')
 const bodyParser = require('body-parser')
 const flatfile = require('./flatfiledb')
+const jose = require('jose');
+
+const {OAuth2Client} = require('google-auth-library');
+const e = require('cors');
+
+// TODO - change the client ID and pass it in with an environment variable
+const CLIENT_ID = "152798730660-61397c89b64orq4a0d0i56p74p0ljks2.apps.googleusercontent.com";
+const client = new OAuth2Client(CLIENT_ID);
+
+const NO_TOKEN_IN_HEADER = "No JWT token was specified in the request header";
+
+async function verify( req ) {
+
+  let idTokenString = null;
+  let idToken = null;
+  if ( req != null && req.headers != null ) {
+    idTokenString = req.headers.authorization;
+  }
+  
+  if ( idTokenString != null ) {
+    idToken = JSON.parse( idTokenString );
+  
+    const ticket = await client.verifyIdToken({
+          idToken: idToken.id_token,
+          audience: CLIENT_ID
+      }).catch( error => { throw( error ) } );
+    
+    /*const payload = ticket.getPayload();
+    const userid = payload['sub'];*/
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+  }
+  else {
+    throw( NO_TOKEN_IN_HEADER );
+  }
+}
 
 const app = express();
 app.use(cors());
@@ -40,14 +76,33 @@ app.get( '/companies', (req, res) => {
 });
 
 app.get( '/all', (req, res) => {
-  const all = {
-    companies : persistence.getAllCompanies(),
-    opportunities : persistence.getAllOpportunities(),
-    people : persistence.getAllPeople(),
-    interactions : persistence.getAllInteractions()
-  };
 
-  res.send( all );
+  verify( req )
+  .then( () =>
+    {
+      const all = {
+        companies : persistence.getAllCompanies(),
+        opportunities : persistence.getAllOpportunities(),
+        people : persistence.getAllPeople(),
+        interactions : persistence.getAllInteractions()
+      };
+    
+      res.send( all );
+    }
+  )
+  .catch(
+    error => {
+      console.log ("OH ERROR BIG TIME" );
+      console.log( error );
+      res.ok = false;
+      res.status = 401;
+      res.statusText = error;
+      const all = {};
+
+      res.send( all );
+    }
+  );
+  
 });
 
 app.listen(port, () => {
